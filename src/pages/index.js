cb45b33656f871ada75b7fc29fe96db26d20aab5
Kasia <kasia@miroartdesign.com>
2020-08-03 18:06:10 -0400

    basic blog built in Gatsby and Graphql

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,60 @@
 import React from "react"
-import { Link } from "gatsby"
-
+import { graphql, Link } from "gatsby"
+import styled from 'styled-components';
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
 
-export default IndexPage
+const BlogLink = styled(Link)`
+  text-decoration: none,
+`;
+const BlogTitle  = styled.h2`
+  margin-bottom: 20px;
+  color: orange;
+`
+
+export default (props) => {
+  console.log(props.data.allMarkdownRemark)
+  return (  //data comes from the query below
+    <Layout>
+      <SEO title="Home" />
+      <div>
+        <h2>Kasia's thoughts: <span>{props.data.allMarkdownRemark.totalCount}</span></h2>
+        
+        {
+          props.data.allMarkdownRemark.edges.map(({ node }) => (
+            <div key={node.id}>
+              <BlogLink to={node.fields.slug}>
+                <BlogTitle>{node.frontmatter.title} - {node.frontmatter.date}</BlogTitle>
+              </BlogLink>
+              <p>{node.excerpt}</p>
+            </div>
+          ))
+        }
+      </div>
+
+    </Layout>
+  )
+}
+
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: {fields: [frontmatter___date], order: DESC }) {
+      totalCount,
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date
+          }
+          excerpt,
+          fields {
+            slug
+          }
+        }
+      }
+    }
+  }
+`
---
+++ src/pages/page-3.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo";
+
+const ThirdPage = () => (
+  <Layout>
+    <SEO title="Page two" />
+    <h1>Hello from the third page!</h1>
+  </Layout>
+)
+
+export default ThirdPage;
